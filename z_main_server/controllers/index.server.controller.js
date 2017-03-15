237fe175a1e3de2c404c1e806aa55f1de101a254
zwmei <541149886@qq.com>
2017-03-16 00:18:26 +0800

    change url

--- z_main_server/controllers/index.server.controller.js
+++ z_main_server/controllers/index.server.controller.js
@@ -7,8 +7,12 @@
 var path = require('path');
 
 exports.homePage = function (req, res, next) {
+  return res.redirect('/site_main/index');
+};
+exports.mainPage = function (req, res, next) {
   return res.sendfile(path.join(__dirname, '../../z_web_root/site_main/index.html'));
 };
+
 exports.homeMapLocation = function (req, res, next) {
   return res.sendfile(path.join(__dirname, '../../z_web_root/site_main/template/map_location.client.view.html'));
 };
--- z_main_server/routes/index.server.route.js
+++ z_main_server/routes/index.server.route.js
@@ -8,5 +8,6 @@ var index = require('../controllers/index');
 
 module.exports = function (app) {
   app.route('/').get(index.homePage);
+  app.route('/site_main/index').get(index.mainPage);
   app.route('/home/map_location').get(index.homeMapLocation);
 };
